5d5fc886a7b8ddf27815ab40a7cb29bab7075453
SebastianG <sebastian.garren@gmail.com>
2017-10-06 12:53:02 -0500

    Added TODOs, completed new_post.html

--- main.py
+++ main.py
@@ -3,39 +3,51 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+#TODO: I still need to create these with Python
 
-class Task(db.Model):
+class Blog_post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
+#Each blog post has a title and a body
+
+    def __init__(self, name, body):
+        self.name = title
+        self.body = body
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/blog', methods=['POST', 'GET'])
 def index():
 
+#TODO: rewrite blogz.html
+#1) The action on blogz routes to '/new_post'
+#2) blogs queries database
+#3) and returns all the Blog_post.name and Blog_post.body
+
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+        post_name = request.form['new_post_name']
+        post_body = request.form['new_post_body']
+        new_post = Blog_post(post_name, post_body)
+        db.session.add(new_post)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
+#TODO:Fix parameters to match blogz.HTML
+
+    posts = Blog_post.query.all()
+    return render_template('blogz.html',title="Get It Done!", 
         tasks=tasks, completed_tasks=completed_tasks)
 
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+@app.route('/new_post', methods=['POST'])
+def new_post():
+
+#TODO:I think this only needs to render new_post.html
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
